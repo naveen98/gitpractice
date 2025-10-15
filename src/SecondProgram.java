0a9207817d8b0c3c59a4fcca416823daaf33277b
naveen98 <naveen.banala98@gmail.com>
2025-10-15 12:25:23 +0530

    iam commit my second updated code

--- src/SecondProgram.java
+++ src/SecondProgram.java
@@ -2,13 +2,22 @@ public class SecondProgram {
 
     public static void main(String[] args) {
 
-        int []num={100,300,500,600,200,400};
+        int []num={100, 300, 500, 600, 200, 400, 300, 100};
 
-        for(int numbers:num){
-            System.out.println(numbers);
+        boolean isduplicate=false;
 
+           for (int i=0;i<num.length;i++){
+
+               for (int j=i+1;j<num.length;j++){
+                   if(num[i]==num[j]) {
+                       System.out.println("Duplicate found " + num[i]);
+                       isduplicate = true;
+                   }
+
+               }
         }
-
-
+           if(!isduplicate) {
+               System.out.println(" No Duplicate found ");
+           }
     }
 }
