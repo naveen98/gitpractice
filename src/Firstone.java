6dc96d4d0e2d9d632ee2b397cb2c3ea036905f9e
naveen98 <naveen.banala98@gmail.com>
2025-10-15 11:41:41 +0530

    iam commit my first code

---
+++ src/Firstone.java
@@ -0,0 +1,16 @@
+public class Firstone {
+    public static void main(String []args){
+
+        String[] words={"Hello","Good Morning","Good EVENING","WELCOME"};
+
+        for (int i=0;i<words.length;i++){
+
+            String w=words[i].toLowerCase();
+            System.out.println(w);
+        }
+
+
+
+
+    }
+}
