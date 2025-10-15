3bd166e6da5889ac744f5ec7c8b5f6362617f5ab
naveen98 <naveen.banala98@gmail.com>
2025-10-15 12:14:31 +0530

    iam commit my first updated code

---
+++ src/SecondProgram.java
@@ -0,0 +1,14 @@
+public class SecondProgram {
+
+    public static void main(String[] args) {
+
+        int []num={100,300,500,600,200,400};
+
+        for(int numbers:num){
+            System.out.println(numbers);
+
+        }
+
+
+    }
+}
