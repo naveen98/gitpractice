52683ef7f941929a8a419c21caca8c02e45daeea
naveen98 <naveen.banala98@gmail.com>
2025-10-15 17:43:29 +0530

    my commit

--- src/Firstone.java
+++ src/Firstone.java
@@ -1,4 +1,14 @@
 public class Firstone {
+
+
+    static void family(String fname,String lname){
+
+        System.out.println("firstname :" +fname + " lastname : " +lname);
+
+
+    }
+
+
     public static void main(String []args){
 
         String[] words={"Hello","Good Morning","Good EVENING","WELCOME"};
@@ -11,6 +21,16 @@ public class Firstone {
             System.out.println("we converted uppercase to lowercase");
 
         }
+
+
+        family("Banala"," Naveen");
+
+        family("Banala"," krishna");
+        family("Banala"," suguna");
+        family("Banala"," raj");
+
+        family("Banala"," naveena");
+        family("Banala"," ruhansi");
 
 
 
